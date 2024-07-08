01752c75cbcb332ca5e1a1a4b94636cca099897c
AlessioGiacconi <s1116262@studenti.univpm.it>
2024-07-08 16:14:49 +0200

    seeding

--- db/db.sql
+++ db/db.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS passages (
     passageId SERIAL PRIMARY KEY,
-    lvl INT NOT NULL,
+    level INT NOT NULL,
     needsDPI BOOLEAN NOT NULL
 );
 
@@ -24,5 +24,28 @@ CREATE TABLE IF NOT EXISTS transits (
     badge INT NOT NULL,
     transitDate TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     isAuthorized BOOLEAN NOT NULL,
-    violationDPI BOOLEAN NOT NULL
+    violationDPI BOOLEAN NOT NULL,
+    FOREIGN KEY (badge) REFERENCES users(badgeId),
+    FOREIGN KEY (passage) REFERENCES passages(passageId)
 );
+
+INSERT INTO users (email, passwd, isAdmin, authLevel, isSuspended, tokens) VALUES
+('alessio@gmail.com', 'password', false, 3, false, 100),
+('francesco@gmail.com', 'password', false, 3, false, 100),
+('carcarlo@gmail.com', 'password', false, 1, true, 100),
+('admin@admin.com', 'password', true, 3, false, 100);
+
+INSERT INTO passages (level, needsDPI) VALUES
+(1, false),
+(2, true),
+(3, true),
+(3, false),
+(1, true);
+
+INSERT INTO transits (passage, badge, transitDate, isAuthorized, violationDPI) VALUES
+(1, 1, '2023-07-01 08:30:00', true, false),
+(2, 1, '2023-07-02 09:15:00', false, true),
+(3, 2, '2023-07-03 10:45:00', true, false),
+(1, 3, '2023-07-04 11:30:00', false, true),
+(4, 4, '2023-07-05 12:00:00', true, false),
+(5, 4, '2023-07-06 13:20:00', true, true);

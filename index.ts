6fada2ac7e784c8101913cc752a0bde4c8b2514c
AlessioGiacconi <s1116262@studenti.univpm.it>
2024-07-08 17:28:53 +0200

    setup index

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = Number(process.env.PORT);
+const host = String(process.env.HOST);
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Bruh");
+});
+
+app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+});

b03ee1a911a0bb16377a5cf431fd16742ad1d971
Francesco Camplese <falcone6675@gmail.com>
2024-07-10 14:04:33 +0200

    aggiunto errorMiddleware

--- middleware/errorMiddleware.ts
+++ middleware/errorMiddleware.ts
@@ -1,6 +1,30 @@
-import { Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { ErrorFactory } from '../factory/Errors';
+import { ErrorEnum } from '../factory/Message';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+// Istanza della factory di messaggi di errore
+const errorFactory: ErrorFactory = new ErrorFactory();
+
+// Middleware per la gestione degli errori
+export function errorHandler(err: ErrorEnum, req: Request, res: Response, next: NextFunction) {
+    const errorResponse = errorFactory.getMessage(err).getResponse();
+    console.log("Error Handler: " + errorResponse.message);
+
+    res.setHeader('Content-Type', errorResponse.type);
+    res.status(errorResponse.status).json({
+        response: errorResponse.message,
+        data: {}
+    });
+};
+
+// Middleware per la gestione di errori non mappati
+export function genericErrorHandler(err: any, req: Request, res: Response, next: NextFunction) {
     console.error(err.stack);
-    res.status(500).json({message: err.message});
+    const errorResponse = errorFactory.getMessage(ErrorEnum.InternalServerError).getResponse();
+    
+    res.setHeader('Content-Type', errorResponse.type);
+    res.status(errorResponse.status).json({
+        response: errorResponse.message,
+        data: {}
+    });
 };
